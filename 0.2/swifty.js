84f5242bb6430f2ae9e3dabf584c30b6e06f0874
Parking Master <parkingmaster@email.com>
2021-12-05 11:40:58 -0500

    Update swifty.js

--- 0.2/swifty.js
+++ 0.2/swifty.js
@@ -135,18 +135,15 @@ function getSource(e) {
         window.newSource = decodeURIComponent(e.data)
     })), window.newSource
 }
-setTimeout((function() {
-    window.swift = window, document.head.innerHTML += "<style>.swift-as-console.swift-console{margin:2px;padding:10px;position:absolute;background:#f9f9f9;color:black;width:50%;left:50%;margin-left:-25%;height:25%;border:2px solid #999}swift{display:none}</style>";
-    for (var e = 0; e < document.querySelectorAll("swift").length; e++) {
-        var t = document.querySelectorAll("swift")[e];
-        if (!(/0.[123456789]/g.test(t.textContent.split("[ swift version ").pop().split(" ]")[0]) && t.textContent.split("[ swift version ").pop().split(" ]")[0].length <= 3)) throw new TypeError("Swift.js can't find version \"" + t.textContent.split("[ swift version ").pop().split(" ]")[0].toString() + '"');
-        try {
-          (new Function(t.textContent.replace(/\)\n/g, ");").replace(/\[ swift version 0.[123456789] \]/, "").replace(/func\(/g, "function(").replace(/variable /g, "var ").replace(/letter /g, "let ").replace(/constant /g, "const").replace(/object \{\}/g, "var object = {}").replace(/\$$/g, "swift").replace(/function /g, "var ").replace(/string /g, "var ").replace(/\#\#\+\*[\s\S]*?\*\/|\#\#\+.*/g, '/*').replace(/\#\#\-\*[\s\S]*?\*\/|\#\#\-.*/g, '*/').replace(/\# \*[\s\S]*?\*\/|\#.*/g, '')))();
-        } catch (err) {
-          document.querySelector(".swift-as-console-line").innerHTML += "Uncaught " + err + "<br />";
-        }
-    }
-}), 10);
+
+setTimeout(function() {
+  for (let i = 0; i < document.querySelectorAll("swift").length; i++) {
+    document.querySelectorAll("swift")[i].style.display = "none";
+    let r = document.createElement("script");
+    r.src = "https://cdn.jsdelivr.net/gh/Parking-Master/Swifty.js@latest/" + String(document.querySelectorAll("swift")[i].getAttribute("version")) + "/engine.js";
+    document.body.appendChild(r);
+  }
+}, 1);
 
 document.body.innerHTML += `
   <div class="swift-as-console swift-console">
